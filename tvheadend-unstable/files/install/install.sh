23efd5f88b1443290bf4db170f8bf6e53949471f
piotrasd <piotr.kasprzak@me.com>
2016-04-09 15:05:34 +0100

    Update install.sh

--- tvheadend-unstable/files/install/install.sh
+++ tvheadend-unstable/files/install/install.sh
@@ -35,7 +35,7 @@ pushd /tmp/
 git clone https://github.com/tvheadend/tvheadend.git 
 popd
 pushd /tmp/tvheadend
-git checkout ac48dc24 
+git checkout master 
 ./configure --enable-libffmpeg_static 
 make 
 make install
